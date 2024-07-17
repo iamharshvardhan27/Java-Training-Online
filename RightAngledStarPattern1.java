be49fed0fb6f4ed5dc7ce450c0f794bf72917a3f
Harshvardhan Ajit Mohite <144557140+iamharshvardhan27@users.noreply.github.com>
2024-07-17 12:17:38 +0530

    Right Angled Star Pattern 1

--- RightAngledStarPattern1.java
+++ RightAngledStarPattern1.java
@@ -28,7 +28,7 @@ Output
 */
 
 import java.util.*;
-public class Main {
+public class RightAngledStarPattern1 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
